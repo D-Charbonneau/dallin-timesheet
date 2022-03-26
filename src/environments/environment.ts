d0915ba30a5d883828a26965e9bb715ee2250055
Dallin Charbonneau <dallin.charbonneau3983@stu.mtec.edu>
2022-03-26 12:18:22 -0600

    Finished Timesheet

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyD9gNuj6DCvo6tly6_Di-MumEM6wdcKsOQ",
+        authDomain: "hr-timesheet-74e9c.firebaseapp.com",
+        projectId: "hr-timesheet-74e9c",
+        storageBucket: "hr-timesheet-74e9c.appspot.com",
+        messagingSenderId: "7021556189",
+        appId: "1:7021556189:web:7e19a2ba40bc16cfce8634",
+        measurementId: "G-R84R3EY38H"
+    },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
